77b61093089e35ad3ff6c2c3af4d832af5e983d6
yalunzhu <yalunzhu@gmail.com>
2025-01-24 23:18:25 -0500

    store screener in db

--- src/index.ts
+++ src/index.ts
@@ -25,9 +25,10 @@ const error_file = fs.createWriteStream("./error.log", {
   flags: "w",
 });
 const headers = {
-  // "User-Agent": randomUserAgent.getRandom(userAgents),
-  "User-Agent":
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+  "User-Agent": randomUserAgent.getRandom(userAgents),
+
+  // "User-Agent":
+  // "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
   Accept:
     "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
   "Accept-Encoding": "gzip, deflate, br, zstd",
